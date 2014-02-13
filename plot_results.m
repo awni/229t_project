987c1dafe0b1d1adc1a644bc48c6a9ac2a3f0d94
qipeng <pengrobertqi@163.com>
2014-02-12 22:59:10 -0800

    no message

--- plot_results.m
+++ plot_results.m
@@ -15,26 +15,26 @@ function plot_results(suffix)
     title(['results' suffix]);
     
     %% PCA approach
-    [u,s,v]=svds(cov(grad),2);
-    disp(diag(s));
-    t = cost';
-    tmp=param*v;
-    tmp = [tmp t(:)];
-
-    hold on;
-    f = 1;
-    for i = 1:floor(size(tmp,1)/points_per_trial),
-        x=tmp((i-1)*points_per_trial+1:i*points_per_trial,:);
-        mesh([x(:,f),x(:,f)],[x(:,f+1),x(:,f+1)],[x(:,end),x(:,end)]);
-    end
+%     [u,s,v]=svds(cov(grad),2);
+%     disp(diag(s));
+%     t = cost';
+%     tmp=param*v;
+%     tmp = [tmp t(:)];
+% 
+%     hold on;
+%     f = 1;
+%     for i = 1:floor(size(tmp,1)/points_per_trial),
+%         x=tmp((i-1)*points_per_trial+1:i*points_per_trial,:);
+%         mesh([x(:,f),x(:,f)],[x(:,f+1),x(:,f+1)],[x(:,end),x(:,end)]);
+%     end
 
     %% tSNE approach
     
-%     tmp = tsne(param);
-%     hold on;
-%     for i = 1:floor(size(tmp,1) / points_per_trial),
-%         mesh([tmp(:,1),tmp(:,1)], [tmp(:,2),tmp(:,2)], [cost(:),cost(:)]);
-%     end
+    tmp = tsne(param);
+    hold on;
+    for i = 1:floor(size(tmp,1) / points_per_trial),
+        mesh([tmp(:,1),tmp(:,1)], [tmp(:,2),tmp(:,2)], [cost(:),cost(:)]);
+    end
 
     drawnow;
     
